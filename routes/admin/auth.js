e68a293e773fab385a407991391aa55c549dd6f4
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-06 08:22:17 -0300

    custom validation
    Custom validation was added for email and passwordConfirmation in auth.js

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -16,22 +16,32 @@ router.get('/signup', (req, res) => {
 });
 
 router.post('/signup', [
-    check('email'),
-    check('password'),
+    check('email')
+        .trim()
+        .normalizeEmail()
+        .isEmail()
+        .custom(async (email) => {
+            const existingUser = await usersRepo.getOneBy({ email });
+            if (existingUser) {
+                throw new Error('Email in use');
+            }
+        }),
+    check('password')
+        .trim()
+        .isLength({ min: 4, max: 20 }),
     check('passwordConfirmation')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .custom((passwordConfirmation, { req }) => {
+            if (passwordConfirmation !== req.body.password) {
+                throw new Error('Passwords must match!')
+            }
+        })
     ], async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors)
 
     const { email, password, passwordConfirmation } = req.body;
-
-    const existingUser = await usersRepo.getOneBy({ email });
-
-    if (existingUser) {
-        return res.send('Email in use');
-    }
-
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords must match')
-    }
 
     // Create a user in our user repo to represent this person
     const user = await usersRepo.create({ email, password });
