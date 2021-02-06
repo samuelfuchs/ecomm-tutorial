0b694c2b831f8e14c66e9ad0c8d9eca93aa3b0a7
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-06 12:43:02 -0300

    more updates

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session')
 const authRouter = require('./routes/admin/auth');
-const app = express()
+
+const app = express();
+
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
