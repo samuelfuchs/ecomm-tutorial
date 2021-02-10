c64e632869035094ffbf090bafeaad50b8f88e2e
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-10 19:40:50 -0300

    more updates

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -7,6 +7,7 @@ const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {
 
@@ -16,13 +17,15 @@ router.get('/admin/products/new', (req, res) => {
     res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-    const errors = validationResult(req);
+router.post(
+    '/admin/products/new', 
+    [requireTitle, requirePrice], 
+    upload.single('image'), 
+    (req, res) => {
+        const errors = validationResult(req);
 
+        console.log(req.file)
 
-    req.on('data', data => {
-        console.log(data.toString())
-    })
     res.send('submitted');
 })
 
