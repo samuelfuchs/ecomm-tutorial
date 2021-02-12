175419b44f4f887242bc407fde7c65ddcc65492d
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-12 10:11:22 -0300

    Add some new products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const multer = require('multer');
 
 const { handleErrors } = require('./middlewares');
@@ -9,25 +9,24 @@ const { requireTitle, requirePrice } = require('./validators');
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {
-
-})
+router.get('/admin/products', (req, res) => {});
 
 router.get('/admin/products/new', (req, res) => {
-    res.send(productsNewTemplate({}));
+  res.send(productsNewTemplate({}));
 });
 
 router.post(
-    '/admin/products/new', 
-    upload.single('image'), 
-    [requireTitle, requirePrice],
-    handleErrors(productsNewTemplate),
-    async (req, res) => {
-        const image = req.file.buffer.toString('base64');
-        const { title, price } = req.body;
-        await productsRepo.create({ title, price, image });
+  '/admin/products/new',
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productsNewTemplate),
+  async (req, res) => {
+    const image = req.file.buffer.toString('base64');
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
 
     res.send('submitted');
-})
+  }
+);
 
 module.exports = router;
