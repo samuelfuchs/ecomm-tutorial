f7f2a4663b94f468e5c7ce011eb6ac4ba9b4d799
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-12 18:15:01 -0300

    updated

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -2,7 +2,7 @@ const { validationResult } = require('express-validator');
 
 module.exports = {
   handleErrors(templateFunc, dataCb) {
-    return (req, res, next) => {
+    return async (req, res, next) => {
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
