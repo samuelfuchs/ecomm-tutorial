14ed5dbd11baf293bac0f49919d224cc6341eb13
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-12 09:43:19 -0300

    custom middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
+
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -23,13 +24,9 @@ router.get('/signup', (req, res) => {
 router.post(
     '/signup', 
     [requireEmail, requirePassword, requirePasswordConfirmation], 
+    handleErrors(signupTemplate);
     async (req, res) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return res.send(signupTemplate({ req, errors }));
-        }
-        const { email, password, passwordConfirmation } = req.body;
+        const { email, password } = req.body;
 
         // Create a user in our user repo to represent this person
         const user = await usersRepo.create({ email, password });
@@ -51,13 +48,8 @@ router.get('/signin', (req, res) => {
 
 router.post('/signin', 
     [requireEmailExist, requireValidPasswordForUser], 
+    handleErrors(signinTemplate);
     async (req, res) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return res.send(signinTemplate({ errors }));
-        }
-
         const { email } = req.body;
 
         const user = await usersRepo.getOneBy({ email });
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const { validationResult } = require('express-validator');
 const multer = require('multer');
 
+const { handleErrors } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
@@ -19,15 +19,10 @@ router.get('/admin/products/new', (req, res) => {
 
 router.post(
     '/admin/products/new', 
-    [requireTitle, requirePrice], 
     upload.single('image'), 
+    [requireTitle, requirePrice],
+    handleErrors(productsNewTemplate),
     async (req, res) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return res.send(productsNewTemplate({ errors }))
-        }
-
         const image = req.file.buffer.toString('base64');
         const { title, price } = req.body;
         await productsRepo.create({ title, price, image });
