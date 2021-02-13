842c006cd6d971da9281dc57bd31393822483d35
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-13 17:12:47 -0300

    some changes

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -7,8 +7,10 @@ module.exports = ({ product, errors }) => {
         <form method="POST">
             <input name="title" value="${product.title}" />
             ${getError(errors, 'title')}
+            
             <input name="price" value="${product.price}" />
             ${getError(errors, 'price')}
+
             <input name="image" type="file" />
             <button>Submit</button>
         </form>
