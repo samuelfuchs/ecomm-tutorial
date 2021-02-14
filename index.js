15259c0ecf166158701d147eb81f6762c6c09453
Samuel Fuchs <fuchsgerson@gmail.com>
2021-02-14 10:20:02 -0300

    setting up cart system

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 const adminProductsRouter = require('./routes/admin/products');
 const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(
 app.use(authRouter);
 app.use(productsRouter);
 app.use(adminProductsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log('Listening');
